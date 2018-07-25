d2ca90fa01c0580096016203a951d86dbf9b69ce
Cris <crisli19971001@gmail.com>
2018-07-25 15:24:58 +0800

    update loginAlert

--- 0725work/js/login.js
+++ 0725work/js/login.js
@@ -1,16 +1,16 @@
-var bg = document.getElementsByClassName('left');
-var a = new Array('url(./image/pic1.png)', 'url(./image/pic2.png)', 'url(./image/pic3.png)');
-var list = document.getElementById('desc').getElementsByTagName('li');
-var i = 0;
+let leftBg = document.getElementsByClassName('left');
+let array = new Array('url(./image/pic1.png)', 'url(./image/pic2.png)', 'url(./image/pic3.png)');
+let list = document.getElementById('desc').getElementsByTagName('li');
 
-var nextMove = function () {
+let nextMove = function () {
+    let i = 0;
     i += 1;
     if (i >= list.length) {
         i = 0;
     }
-    bg[0].style.backgroundImage = a[i];
+    leftBg[0].style.backgroundImage = array[i];
     list[i].style.background = '#fff';
-    for (var j = 0; j < 3; j++) {
+    for (let j = 0; j < 3; j++) {
         if (j != i) {
             list[j].style.background = '#555';
         }
@@ -18,14 +18,14 @@ var nextMove = function () {
 };
 setInterval(nextMove, 3000);
 
-var loginTest = function () {
-    var user = document.getElementById('username').value;
-    var pwd = document.getElementById('password').value;
-    console.log(pwd.length);
-    if((user != "") && (pwd.length >= 6)) {
-        alert("登录成功，你好 " + user);
-    }
-    else {
-        alert("登录失败，用户名不能为空且密码不能少于6位数");
+let loginTest = function () {
+    const user = document.getElementById('username').value;
+    const pwd = document.getElementById('password').value;
+    const alertP = document.getElementById('alert');
+    alertP.style.display = 'block';
+    if ((user != "") && (pwd.length >= 6)) {
+        alertP.innerHTML = `登录成功，你好 ${user}`;
+    } else {
+        alertP.innerHTML = `登录失败，用户名不能为空且密码不能少于6位数`;
     }
 }
